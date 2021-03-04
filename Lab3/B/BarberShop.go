db614d81553341906973b8235730c34bc3a79620
ozzwoy <dima.pashenko.200081@gmail.com>
2021-03-04 10:10:02 +0200

    Fixed bugs with time

--- Lab3/B/BarberShop.go
+++ Lab3/B/BarberShop.go
@@ -10,11 +10,11 @@ import (
 func barber(queue chan int, hairdoDone chan bool) {
 	fmt.Println("Barber opens barbershop.")
 	for id := range queue {
-		fmt.Println("Barber invites client " + strconv.Itoa(id) + ".")
-		time.Sleep(1000)
+		fmt.Println("\nBarber invites client " + strconv.Itoa(id) + ".")
+		time.Sleep(time.Second)
 		fmt.Println("Barber finishes hairdo and sees client #" + strconv.Itoa(id) + " off.")
 		hairdoDone <- true
-		time.Sleep(100)
+		time.Sleep(time.Millisecond * 100)
 	}
 	fmt.Println("Barber closes barbershop.")
 }
@@ -22,10 +22,10 @@ func barber(queue chan int, hairdoDone chan bool) {
 func client(id int, queue chan int, hairdoDone chan bool, waitGroup* sync.WaitGroup)  {
 	fmt.Println("Client #" + strconv.Itoa(id) + " takes place in queue to barbershop.")
 	queue <- id
-	time.Sleep(10)
+	time.Sleep(time.Millisecond * 100)
 	fmt.Println("Client #" + strconv.Itoa(id) + " sits in chair and falls asleep.")
 	<- hairdoDone
-	fmt.Println("Client #" + strconv.Itoa(id) + " wakes up and leaves barbershop.\n")
+	fmt.Println("Client #" + strconv.Itoa(id) + " wakes up and leaves barbershop.")
 	waitGroup.Done()
 }
 
@@ -42,7 +42,7 @@ func main() {
 		go client(i, queue, hairdoDone, &waitGroup)
 	}
 
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 3)
 
 	for i := numOfClients / 2; i < numOfClients; i++ {
 		go client(i, queue, hairdoDone, &waitGroup)
@@ -50,5 +50,5 @@ func main() {
 
 	waitGroup.Wait()
 	close(queue)
-	time.Sleep(100)
+	time.Sleep(time.Millisecond * 100)
 }
